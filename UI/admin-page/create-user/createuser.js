e5fd5a788b2a8c0375220f5156ac7ae1579124aa
Parikshith <paari333@gmail.com>
2019-10-08 23:23:29 -0700

    Modified javascript to accomodate REST API Calls

--- UI/admin-page/create-user/createuser.js
+++ UI/admin-page/create-user/createuser.js
@@ -1,19 +1,48 @@
-function checkForStudent() {
-    var roles = document.getElementById("selectRole");
-    var selectedVal = roles.options[roles.selectedIndex].value;
-    if (selectedVal === "student") {
+
+function checkForStudent(selectedVal) {
+    if (selectedVal.value === "student") {
         document.getElementById("labelGrade").style.display = "block";
         document.getElementById("selectGrade").style.display = "block";
+		//document.getElementById("selectGrade").required = true;
     } else {
         document.getElementById("labelGrade").style.display = "none";
         document.getElementById("selectGrade").style.display = "none";
+		//document.getElementById("selectGrade").required = false;
     }
-
 }
 function register() {
-    alert("User successfully registered!");
-    location.reload();
+	var reqFields = ["userid","name","psw","psw-repeat","selectRole"];
+	var nonNullFields = 0;
+	for (nonNullFields=0; nonNullFields<reqFields.length; nonNullFields++) {
+		if (document.getElementById(reqFields[nonNullFields]).value === '' || document.getElementById(reqFields[nonNullFields]).value === null) {
+			return false;
+		}
+	}
+	if (document.getElementById("email").value !== '' && document.getElementById("email").value !== null) {
+		var isValidEmail = validateEmail(document.getElementById("email").value);
+		if (!isValidEmail) {
+			alert("Invalid e-mail id entered");
+			return false;
+		}
+	}
+	if (document.getElementById("psw").value !== document.getElementById("psw-repeat").value) {
+		alert("Passwords do not match");
+		return false;
+	}
+	if (nonNullFields===reqFields.length) {
+		//call REST API here
+		
+		alert("User successfully registered!");
+		return true;
+	} 
+}
+function regUserAPICall() {
+	
+}
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 function cancelreg() {
-    location.reload();
+    window.location.href="../";
 }
--- UI/indexjs.js
+++ UI/indexjs.js
@@ -1,6 +1,18 @@
 function login() {
-
+	//REST API call
+	//capture response
+	/* Temporarily Username-admin will route to admin portal
+	*  other users will route to student portal i.e drag and drop page
+	*/
+	if(document.getElementById("username").value === 'admin') {
+		window.location.href="admin-page";
+	} else {
+		window.location.href="landing-page";
+	}
+	
 }
 function logout() {
-    window.location.href = "../../UI/index.html";
+	if (confirm("Are you sure to logout?")) {
+		document.location.href="/UI/";
+	}
 }
