a2a767015861e1309f12f7dc3bbc832cf6d54a8d
Nagarjun1504 <47918674+Nagarjun1504@users.noreply.github.com>
2019-10-09 17:59:39 -0700

    POST API For User Registration by Administrator

---
+++ REST/src/main/java/com/asu/ser515/team20/mathboard/controller/UserRegistrationController.java
@@ -0,0 +1,35 @@
+package com.asu.ser515.team20.mathboard.controller;
+
+import com.asu.ser515.team20.mathboard.model.User;
+import com.asu.ser515.team20.mathboard.service.UserRegistrationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(tags = {"User Registration"})
+@SwaggerDefinition(tags = {
+        @Tag(name = "Swagger Resource", description = "User Registration Swagger")})
+public class UserRegistrationController {
+
+    @Autowired
+    private UserRegistrationService userRegistrationService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/RegisterUser")
+    @ApiOperation(value = "Provide a user to be registered in JSON format",response = ResponseEntity.class)
+    public ResponseEntity<String> getUser(@RequestBody User user){
+        userRegistrationService.addUser(user);
+        return new ResponseEntity<>("User Added Successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getUser/{userId}", method = RequestMethod.GET)
+    @ApiOperation(value = "Provide userId",response = User.class)
+    public User ExpressionEvaluator(@PathVariable String userId) {
+        return userRegistrationService.getUsers(userId);
+    }
+}
